19f634067de4f54afd0522f94c569e72c9eb44d3
U104547036 <ko.nakajima@diamondhead.tech>
2021-08-27 16:18:58 +0900

    ｆｄｓ

--- main.go
+++ main.go
@@ -1,77 +1,102 @@
+// package main
+
+// import (
+// 	// for debuging
+// 	"fmt"
+// 	"os"
+// 	// "reflect"
+// 	"github.com/gin-gonic/gin"
+// 	"github.com/gin-contrib/cors"
+// 	"time"
+// 	// "github.com/joho/godotenv"
+	
+
+// 	"db"
+// 	"handlers"
+// 	"models"
+// 	"lib"
+// )
+
+// func setupRouter() *gin.Engine {
+
+// 	fmt.Println("OKOKOK")
+
+// 	r := gin.Default()
+// 	// r.Use(cors.Default())
+// 	r.Use(cors.New(cors.Config{
+// 		AllowOrigins: []string{
+// 			"http://localhost:8010",
+// 		},
+// 		AllowMethods: []string{
+// 			"POST",
+// 			"DELETE",
+// 			"OPTIONS",
+// 		},
+// 		AllowHeaders: []string{
+// 			"Origin",
+// 			"Access-Control-Allow-Credentials",
+// 			"Access-Control-Allow-Headers",
+// 			"Content-Type",
+// 			"Content-Length",
+// 			"Accept-Encoding",
+// 			"Authorization",
+// 			"X-CSRFToken",
+// 		},
+// 		AllowCredentials: true,
+// 		MaxAge: 24 * time.Hour,
+// 	  }))
+// 	// log
+// 	log := r.Group("/log")
+// 	log.Use(lib.AuthHeader())
+// 	{
+// 		log.GET("/list/:userId", handlers.GetLogList)
+// 		log.POST("/create/:userId", handlers.CreateLog)
+// 		log.POST("/delete/:logId", handlers.DeleteLog)
+// 	}
+// 	// tenant.Use(common.AuthHeader())
+
+// 	return r
+// }
+
+// var router = setupRouter()
+
+// func main() {
+// 	os.Setenv("PORT", "8011")
+// 	// err := godotenv.Load(fmt.Sprintf("envfiles/%s.env", os.Getenv("GO_ENV")))
+// 	// if err != nil {
+// 	// 	fmt.Printf("読み込み出来ませんでした: %v", err)
+// 	// } 
+// 	port := os.Getenv("PORT")
+// 	mysql := db.Init()
+// 	mysql.AutoMigrate(&models.Log{})
+// 	defer mysql.Close()
+// 	fmt.Println(port, "WE")
+
+// 	r := router
+// 	r.Run(":"+port)
+// }
 package main
 
 import (
-	// for debuging
-	"fmt"
+    "log"
 	"os"
-	// "reflect"
-	"github.com/gin-gonic/gin"
-	"github.com/gin-contrib/cors"
-	"time"
-	// "github.com/joho/godotenv"
-	
 
-	"db"
-	"handlers"
-	"models"
-	"lib"
+    "github.com/gin-gonic/gin"
 )
-
-func setupRouter() *gin.Engine {
-
-	fmt.Println("OKOKOK")
-
-	r := gin.Default()
-	// r.Use(cors.Default())
-	r.Use(cors.New(cors.Config{
-		AllowOrigins: []string{
-			"http://localhost:8010",
-		},
-		AllowMethods: []string{
-			"POST",
-			"DELETE",
-			"OPTIONS",
-		},
-		AllowHeaders: []string{
-			"Origin",
-			"Access-Control-Allow-Credentials",
-			"Access-Control-Allow-Headers",
-			"Content-Type",
-			"Content-Length",
-			"Accept-Encoding",
-			"Authorization",
-			"X-CSRFToken",
-		},
-		AllowCredentials: true,
-		MaxAge: 24 * time.Hour,
-	  }))
-	// log
-	log := r.Group("/log")
-	log.Use(lib.AuthHeader())
-	{
-		log.GET("/list/:userId", handlers.GetLogList)
-		log.POST("/create/:userId", handlers.CreateLog)
-		log.POST("/delete/:logId", handlers.DeleteLog)
-	}
-	// tenant.Use(common.AuthHeader())
-
-	return r
-}
-
-var router = setupRouter()
 
 func main() {
 	os.Setenv("PORT", "8011")
-	// err := godotenv.Load(fmt.Sprintf("envfiles/%s.env", os.Getenv("GO_ENV")))
-	// if err != nil {
-	// 	fmt.Printf("読み込み出来ませんでした: %v", err)
-	// } 
-	port := os.Getenv("PORT")
-	mysql := db.Init()
-	mysql.AutoMigrate(&models.Log{})
-	defer mysql.Close()
-	fmt.Println(port, "WE")
+	err := godotenv.Load(fmt.Sprintf("envfiles/%s.env", os.Getenv("GO_ENV")))
+	if err != nil {
+		fmt.Printf("読み込み出来ませんでした: %v", err)
+	} 
+    router := gin.Default()
 
-	r := router
-	r.Run(":"+port)
+    router.GET("/hello", func(ctx *gin.Context) {
+        ctx.String(200, "Hello World")
+    })
+	port := os.Getenv("PORT")
+    if err := router.Run(":"+port); err != nil {
+        log.Fatal("Server Run Failed.: ", err)
+    }
 }
