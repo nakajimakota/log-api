dc26b116a934d698d1cb866b49b8e46e39d85f97
nakajimakota <nakajimakouta.cs@gmail.com>
2021-08-29 13:19:02 +0900

    OK

--- db/main.go
+++ db/main.go
@@ -2,8 +2,10 @@ package db
 
 import (
 	// "fmt"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres" // Use PostgreSQL in gorm
+	"github.com/lib/pq"
 )
 
 type Database struct {
@@ -13,7 +15,15 @@ type Database struct {
 var DB *gorm.DB
 
 func Init() *gorm.DB {
-	conn, err := gorm.Open("postgres", "host=db port=5432 user=user dbname=go-log-api password=user sslmode=disable")
+	// conn, err := gorm.Open("postgres", "host=db port=5432 user=user dbname=go-log-api password=user sslmode=disable")
+	url := "postgres://qhpyygbsdauxkr:cad56e5a1b0b1d118987fb8941090713803c61d911eae9b209671854bab37388@ec2-35-171-250-21.compute-1.amazonaws.com:5432/d1sih1b00rn14v"
+	connection, err := pq.ParseURL(url)
+	if err != nil {
+		panic(err.Error())
+	}
+	connection += " sslmode=require"
+
+	conn, err := gorm.Open("postgres", connection)
 	if err != nil {
 		panic("データベース接続に失敗しました")
 	}
