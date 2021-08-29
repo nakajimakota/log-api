aa336b3a3eba6c3b6cc4c508280b76e1b07ce095
nakajimakota <nakajimakouta.cs@gmail.com>
2021-08-30 07:55:09 +0900

    OK

--- db/main.go
+++ db/main.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres" // Use PostgreSQL in gorm
+	"github.com/lib/pq"
 )
 
 type Database struct {
@@ -19,22 +20,22 @@ func Init() *gorm.DB {
 	fmt.Println(("OK"))
 	// ENVIRONMENT := os.Getenv("ENVIRONMENT")
 	// if ENVIRONMENT == "local" {
-	conn, err := gorm.Open("postgres", "host=db port=5432 user=user dbname=go-log-api password=user sslmode=disable")
-	DB = conn
-	if err != nil {
-		panic("データベース接続に失敗しました")
-	}
+	// conn, err := gorm.Open("postgres", "host=db port=5432 user=user dbname=go-log-api password=user sslmode=disable")
+	// DB = conn
+	// if err != nil {
+	// 	panic("データベース接続に失敗しました")
+	// }
 	// }
 	// if ENVIRONMENT == "dev" {
-	// url := "postgres://qhpyygbsdauxkr:cad56e5a1b0b1d118987fb8941090713803c61d911eae9b209671854bab37388@ec2-35-171-250-21.compute-1.amazonaws.com:5432/d1sih1b00rn14v"
-	// connection, err := pq.ParseURL(url)
-	// if err != nil {
-	// 	panic(err.Error())
-	// }
-	// connection += " sslmode=require"
+	url := "postgres://qhpyygbsdauxkr:cad56e5a1b0b1d118987fb8941090713803c61d911eae9b209671854bab37388@ec2-35-171-250-21.compute-1.amazonaws.com:5432/d1sih1b00rn14v"
+	connection, err := pq.ParseURL(url)
+	if err != nil {
+		panic(err.Error())
+	}
+	connection += " sslmode=require"
 
-	// conn, err := gorm.Open("postgres", connection)
-	// DB = conn
+	conn, err := gorm.Open("postgres", connection)
+	DB = conn
 	// }
 
 	return DB
